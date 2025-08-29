feedf9901e3165eacf8aa4d2b4793d192dcc42c8
IsaacBenavides <isaac.3javier@gmail.com>
2025-08-29 00:45:12 -0600

    Ajustes para el uso mobil

--- src/main/java/cr/ac/ucr/cicloVital/config/SecurityConfig.java
+++ src/main/java/cr/ac/ucr/cicloVital/config/SecurityConfig.java
@@ -3,17 +3,21 @@ package cr.ac.ucr.cicloVital.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .formLogin(login -> login.disable())
-                .httpBasic(basic -> basic.disable());
+                .cors(cors -> {}) // usa el bean CorsConfigurationSource
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/publico/**").permitAll()
+                        .anyRequest().permitAll() // ajusta según tu caso
+                );
         return http.build();
     }
 }
