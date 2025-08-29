b876751e6e4abed2d16548cfe56242a678c891d1
IsaacBenavides <isaac.3javier@gmail.com>
2025-08-29 13:37:14 -0600

    Ajustes

--- src/main/java/cr/ac/ucr/cicloVital/config/SecurityConfig.java
+++ src/main/java/cr/ac/ucr/cicloVital/config/SecurityConfig.java
@@ -3,21 +3,17 @@ package cr.ac.ucr.cicloVital.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> {}) // usa el bean CorsConfigurationSource
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/publico/**").permitAll()
-                        .anyRequest().permitAll() // ajusta según tu caso
-                );
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin(login -> login.disable())
+                .httpBasic(basic -> basic.disable());
         return http.build();
     }
 }
